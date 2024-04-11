78b4fd40660545a28e975c08cd0cf47fe01425da
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-04-11 11:17:21 +0200

    added input and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
@@ -6,29 +6,41 @@ function App() {
 const [pokemon, setPokemon] = useState("pikachu");
 const [pokemonData, setPokemonData] = useState([]);
 
+const inputRef = useRef<HTMLInputElement | null>(null);
+
 const getPokemon = async () => {
-  const url =`https://pokeapi.co/api/v2/${pokemon}`;
+  try {
+  const url =`https://pokeapi.co/api/v2/pokemon/${pokemon}`;
   const response = await fetch(url);
   const result = await response.json();
   console.log(result);
   const toArray : any = [];
-  toArray.push(result);
+  toArray.push(result, ...pokemonData);
   setPokemonData(toArray);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const handleClick = () => {
+  setPokemon(inputRef.current!.value);
 }
 
 useEffect(() => {
   getPokemon();
-},[pokemon])
+},[pokemon]);
 
 
   return (
     <>
-  {pokemonData.map((pokemon: any) => (
-  <div key={pokemon.id}>
-    <img src={pokemon.sprites.front_default}></img>
-    <p>Name: {pokemon.name}</p>
-    </div>
-  ))}
+    <input ref={inputRef} defaultValue={"Pikachu"}></input>
+    <button onClick={() => handleClick()}>Get pokemon</button>
+      {pokemonData.map((pokemon: any) => (
+      <div key={pokemon.id}>
+        <img src={pokemon.sprites.front_default}></img>
+        <p>Name: {pokemon.name}</p>
+        </div>
+      ))}
        
     </>
   )
