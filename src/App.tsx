d4c7abbe970261bfcdaed703b4e84cb5ed02813c
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-04-11 11:42:54 +0200

    using no useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -3,47 +3,40 @@ import './App.css'
 
 function App() {
 
-const [pokemon, setPokemon] = useState("pikachu");
-const [pokemonData, setPokemonData] = useState([]);
+  const [pokemonData, setPokemonData] = useState([]);
 
-const inputRef = useRef<HTMLInputElement | null>(null);
+  const inputRef = useRef<HTMLInputElement | null>(null);
 
-const getPokemon = async () => {
-  try {
-  const url =`https://pokeapi.co/api/v2/pokemon/${pokemon}`;
-  const response = await fetch(url);
-  const result = await response.json();
-  console.log(result);
-  const toArray : any = [];
-  toArray.push(result, ...pokemonData);
-  setPokemonData(toArray);
-  } catch (error) {
-    console.error(error);
+  const getPokemon = async (pokemon: string) => {
+    try {
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+    const response = await fetch(url);
+    const result = await response.json();
+    console.log(result);
+    const toArray: any = [];
+    toArray.push(result, ...pokemonData);
+    setPokemonData(toArray);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleClick = () => {
+    getPokemon(inputRef.current!.value);
   }
-};
-
-const handleClick = () => {
-  setPokemon(inputRef.current!.value);
-}
-
-useEffect(() => {
-  getPokemon();
-},[pokemon]);
-
 
   return (
     <>
-    <input ref={inputRef} defaultValue={"Pikachu"}></input>
-    <button onClick={() => handleClick()}>Get pokemon</button>
+      <input ref={inputRef} defaultValue={"pikachu"}></input>
+      <button onClick={() => handleClick()}>Get pokemon</button>
       {pokemonData.map((pokemon: any) => (
-      <div key={pokemon.id}>
-        <img src={pokemon.sprites.front_default}></img>
-        <p>Name: {pokemon.name}</p>
+        <div key={pokemon.id}>
+          <img src={pokemon.sprites.front_default}></img>
+          <p>Name: {pokemon.name}</p>
         </div>
       ))}
-       
     </>
-  )
+  );
 }
 
 export default App
